ca185593e31991f5eb6cbdfd0754b584c35ed1f2
Jonathan Harker <jonathan@catalyst.net.nz>
2021-11-23 09:20:45 +1300

    Fix global  error when using the tester page

--- classes/auth.php
+++ classes/auth.php
@@ -1076,6 +1076,7 @@ class auth extends \auth_plugin_base {
      * A simple GUI tester which shows the raw API output
      */
     public function test_settings() {
+        global $PAGE;
         include(__DIR__.'/../tester.php');
     }
 
